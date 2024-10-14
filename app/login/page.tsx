39495951ca81eb0ef39e7cbd65ca928442b476ac
Mehdi Yanat <hamar3106@gmail.com>
2024-10-14 04:37:05 +0100

    user can signup and login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,35 +2,82 @@
 'use client'
 import { ColorModeContext } from '@/providers/ThemeProvider';
 import { LoginFormData } from '@/types';
-import { Input, Layout } from 'antd'
+import { Input, Spin } from 'antd'
 import Link from 'next/link'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
-import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import { EyeInvisibleOutlined, EyeTwoTone, LoadingOutlined } from '@ant-design/icons';
+import { useRouter } from 'next/navigation';
+import { AuthContext } from '@/providers/AuthProvider';
+import { getUsers } from '@/utils/client/localStorage';
 
 
-const login = () => {
+const Login: React.FC = () => {
+
+
+    const authContext = useContext(AuthContext);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+    const router = useRouter();
 
     const {
         control,
         handleSubmit,
         formState: { errors },
-        watch,
     } = useForm<LoginFormData>({
         mode: 'onTouched', // Validate fields as they are touched
     });
 
     const onSubmit: SubmitHandler<LoginFormData> = (data) => {
-        console.log('Login Data:', data);
-        // TODO: Handle login logic, e.g., send data to an API
+        setLoading(true);
+        setError(null);
+
+        const email = data.email;
+        const password = data.password;
+
+        // Simulate an API call
+        setTimeout(() => {
+            const storedUser = getUsers()
+
+            if (storedUser.length > 0) {
+                const user = storedUser.find(user => user.email === email && user.password === password);                
+
+                if (user?.email) {
+                    // Successful login
+                    authContext?.login(user);
+                    router.push('/'); // Redirect to dashboard or any other page
+                } else {
+                    setError('Invalid email or password');
+                    setLoading(false)
+                }
+            } else {
+                // Failed login
+                setError('Invalid email or password');
+                setLoading(false)
+            }
+        }, 500); // Simulate a delay for the API call
+
     };
+
+
     const { mode } = useContext(ColorModeContext);
 
+    if (authContext?.isAuthenticated) {
+        return (
+            <div className='flex h-screen bg-gray-900 items-center justify-center'>
+                <Spin size="large" indicator={<LoadingOutlined spin />} />
+            </div>
+        );
+    }
+
+
     return (
-        <section className={`${mode !== "dark" ? "bg-white" : "bg-gray-900"}`}>
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <div className={`w-full  ${mode === "dark" ? "bg-white" : "bg-gray-900"} rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 `}>
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+        <section className={`${mode !== "dark" ? "bg-white" : "bg-gray-900"} h-screen flex items-center`}>
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto w-full md:h-screen lg:py-0">
+                <div className={`w-full  ${mode === "dark" ? "bg-white" : "bg-gray-900"} min-h-[372px] rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 `}>
+                    {loading ? <div className='flex h-[372px] items-center justify-center' >
+                        <Spin size="large" indicator={<LoadingOutlined spin />} />
+                    </div> : <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 className={`text-xl font-bold leading-tight tracking-tight ${mode === "dark" ? "text-gray-900" : "text-white"} md:text-2xl `}>
                             Sign in to your account
                         </h1>
@@ -105,15 +152,20 @@ const login = () => {
                                 )}
                             </div>
                             <button type='submit' className={`w-full ${mode === "dark" ? "text-gray-900" : "text-white"}  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800`}>Sign in</button>
+                            {error && (
+                                <p className="text-red-600 text-sm mt-1">
+                                    {error}
+                                </p>
+                            )}
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                 Don’t have an account yet? <Link href="/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
                             </p>
                         </form>
-                    </div>
+                    </div>}
                 </div>
             </div>
         </section>
     )
 }
 
-export default login
+export default Login
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,26 +1,60 @@
 'use client';
 import { ColorModeContext } from '@/providers/ThemeProvider';
-import { Input } from 'antd';
+import { Input, Spin } from 'antd';
 import Link from 'next/link';
-import React, { useContext } from 'react';
-import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import React, { useContext, useEffect, useState } from 'react';
+import { EyeInvisibleOutlined, EyeTwoTone, LoadingOutlined } from '@ant-design/icons';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
-import { FormData } from '@/types';
+import { FormData, User } from '@/types';
+import { findUserByEmail, saveUser } from '@/utils/client/localStorage';
+import { useRouter } from 'next/navigation';
+import { AuthContext } from '@/providers/AuthProvider';
 
 const Signup: React.FC = () => {
-    // 2. Type Your `useForm` Hook
+
+    const router = useRouter()
+
+    const authContext = useContext(AuthContext);
+    const [signupError, setSignupError] = useState<string | null>(null);
+    const [signupSuccess, setSignupSuccess] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(false); // Loading state
+
+
     const {
         control,
         handleSubmit,
         formState: { errors },
         watch,
+        reset
     } = useForm<FormData>({
         mode: 'onTouched', // Validate fields as they are touched
     });
 
     const onSubmit: SubmitHandler<FormData> = (data) => {
-        console.log('Form Data:', data);
-        // TODO: Handle form submission (e.g., send data to an API)
+        setLoading(true); // Start loading
+
+        // Simulating async operation with setTimeout
+        setTimeout(() => {
+            const existingUser = findUserByEmail(data.email);
+            if (existingUser) {
+                setSignupError('A user with this email already exists.');
+                setSignupSuccess(null);
+                setLoading(false); // Stop loading
+                return;
+            }
+
+            // Create new user
+            const newUser: User = {
+                email: data.email,
+                password: data.password,
+            };
+
+            saveUser(newUser);
+            setSignupSuccess('Signup successful! You can now sign in.');
+            setSignupError(null);
+            reset();
+            setLoading(false); // Stop loading
+        }, 500); // Delay in milliseconds (adjust as needed)
     };
 
     const { mode } = useContext(ColorModeContext);
@@ -28,11 +62,32 @@ const Signup: React.FC = () => {
     // Watching password value for confirmation validation
     const password = watch('password', '');
 
+    useEffect(() => {
+        if (signupSuccess) {
+            router.push('/login')
+        }
+
+        if (authContext?.isAuthenticated) {
+            router.push('/')
+        }
+    }, [signupSuccess, authContext?.isAuthenticated, router])
+
+
+    if (authContext?.isAuthenticated) {
+        return (
+            <div className='flex h-screen bg-gray-900 items-center justify-center'>
+                <Spin size="large" indicator={<LoadingOutlined spin />} />
+            </div>
+        );
+    }
+
     return (
-        <section className={`${mode !== 'dark' ? 'bg-white' : 'bg-gray-900'}`}>
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <div className={`w-full ${mode === 'dark' ? 'bg-white' : 'bg-gray-900'} rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0`}>
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+        <section className={`${mode !== 'dark' ? 'bg-white' : 'bg-gray-900'} h-screen flex items-center`}>
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto  w-full md:h-screen lg:py-0">
+                <div className={`w-full ${mode === 'dark' ? 'bg-white' : 'bg-gray-900'}  rounded-lg min-h-[456px] shadow md:mt-0 sm:max-w-md xl:p-0`}>
+                    {loading ? <div className='flex border h-[456px] w-full items-center justify-center' >
+                        <Spin size="large" indicator={<LoadingOutlined spin />} />
+                    </div> : <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 className={`text-xl font-bold leading-tight tracking-tight ${mode === 'dark' ? 'text-gray-900' : 'text-white'} md:text-2xl`}>
                             Create new account
                         </h1>
@@ -154,11 +209,16 @@ const Signup: React.FC = () => {
                             <button type='submit' className={`w-full ${mode === 'dark' ? 'text-gray-900' : 'text-white'} focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center`}>
                                 Sign up
                             </button>
+                            {signupError && (
+                                <p className="text-red-600 text-sm mt-1">
+                                    {signupError}
+                                </p>
+                            )}
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                 Already have an account? <Link href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign in</Link>
                             </p>
                         </form>
-                    </div>
+                    </div>}
                 </div>
             </div>
         </section>
